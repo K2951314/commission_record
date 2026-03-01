23c85da73e64545c4d2237d3ab35ea21a659a1b1
尘思 <k2951314@163.com>
2026-03-01 14:06:43 +0800

    fix(build): avoid importing package in setup.py for isolated editable builds

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
 
-# get version from __version__ variable in commission_record/__init__.py
-from commission_record import __version__ as version
-
 setup(
     name="commission_record",
-    version=version,
+    version="0.0.1",
     description="用于记录额外的分成",
     author="舍满取半",
     author_email="qdsmqb@163.com",
